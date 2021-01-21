80ce352f2be470916efa5a3ecd7349bdef0a0165
fgonzarenas <fgonzarenas@gmail.com>
2021-01-21 14:50:07 +0100

    maven

---
+++ src/main/java/translator/GUI.java
@@ -0,0 +1,22 @@
+package translator;
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+
+public class GUI 
+{
+	public static void main(String args[]) 
+	{
+		System.setProperty("org.graphstream.ui", "swing");
+		
+		Graph graph = new SingleGraph("Tutorial 1");
+
+		graph.addNode("A");
+		graph.addNode("B");
+		graph.addNode("C");
+		graph.addEdge("AB", "A", "B");
+		graph.addEdge("BC", "B", "C");
+		graph.addEdge("CA", "C", "A");
+
+		graph.display();	
+	}
+}
