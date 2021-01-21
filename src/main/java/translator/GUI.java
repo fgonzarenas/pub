74011b95558f69b4e99772c3161ce3f3f1e7bf4c
fgonzarenas <fgonzarenas@gmail.com>
2021-01-21 16:18:11 +0100

    stylesheet

--- src/main/java/translator/GUI.java
+++ src/main/java/translator/GUI.java
@@ -1,14 +1,22 @@
 package translator;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
+import org.graphstream.ui.swing_viewer.ViewPanel;
+import org.graphstream.ui.view.Viewer;
 
 public class GUI 
 {
-	public static void main(String args[]) 
+	private Graph graph;
+	
+	public GUI(String file)
+	{
+		graph = new MultiGraph(file);
+	}
+	
+	public void init()
 	{
 		System.setProperty("org.graphstream.ui", "swing");
-		Graph graph = new MultiGraph("Le Havre");
-			
+		
 		try
 		{
 			graph.read("road-networks/LeHavre.dgs");
@@ -19,6 +27,25 @@ public class GUI
 			System.exit(1);
 		}
 			
-		graph.display(false);   // No auto-layout.
+		init_stylesheet();
+		
+		Viewer viewer = graph.display(false);   // No auto-layout.
+		/*ViewPanel view = (ViewPanel) viewer.getDefaultView(); // ViewPanel is the view for gs-ui-swing
+		//view.resizeFrame(800, 600);
+		view.getCamera().setViewCenter(3000, 8000, 0);
+		view.getCamera().setViewPercent(0.25);*/
+	}
+	
+	public void init_stylesheet()
+	{
+		graph.setAttribute("ui.quality");
+		graph.setAttribute("ui.antialias");
+		graph.setAttribute("ui.stylesheet", "url('file://stylesheet')");
+	}
+	
+	public static void main(String args[]) 
+	{	
+		GUI view = new GUI("LeHavre.dgs");
+		view.init();
 	}
 }
